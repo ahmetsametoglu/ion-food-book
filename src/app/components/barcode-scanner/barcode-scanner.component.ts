30b4be3adea7466d2b82a4e214045a32d68b5eca
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-01-15 08:17:59 +0100

    feat: create barcode scanner component

---
+++ src/app/components/barcode-scanner/barcode-scanner.component.ts
@@ -0,0 +1,169 @@
+import {
+  Component,
+  Input,
+  Output,
+  ViewChild,
+  OnDestroy,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  EventEmitter,
+} from '@angular/core';
+import * as Quagga from 'quagga';
+import { mapToReader } from './barcode-types';
+import { DEFAULT_CONFIG, QuaggaConfig } from './stream-config';
+
+@Component({
+  selector: 'app-barcode-scanner',
+  templateUrl: './barcode-scanner.component.html',
+  styleUrls: ['./barcode-scanner.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class BarcodeScannerComponent implements OnChanges, OnDestroy {
+  // Inputs
+  @Input() type: string | string[];
+
+  @Input() deviceId: string;
+
+  // Outputs
+  @Output() valueChanges = new EventEmitter();
+
+  @Output() started = new EventEmitter();
+
+  @ViewChild('BarcodeScanner', { static: true }) barcodeScanner;
+
+  private _started = false;
+
+  get isStarted() {
+    return this._started;
+  }
+
+  private configQuagga: QuaggaConfig = DEFAULT_CONFIG;
+
+  ngOnDestroy(): void {
+    this.stop();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.restart();
+  }
+
+  private _init() {
+    return new Promise((resolve, reject) => {
+      Quagga.onProcessed(result => this.onProcessed(result));
+
+      Quagga.onDetected(result => this.onDetected(result));
+
+      this.configQuagga.inputStream.target = this.barcodeScanner.nativeElement;
+
+      if (this.type) {
+        this.configQuagga.decoder.readers = mapToReader(this.type);
+      }
+
+      if (this.deviceId) {
+        this.configQuagga.inputStream.constraints.deviceId = this.deviceId;
+      }
+
+      Quagga.init(this.configQuagga, err => {
+        if (err) {
+          console.log(err);
+          return reject(err);
+        }
+
+        resolve();
+      });
+    });
+  }
+
+  async start() {
+    if (!this._started) {
+      await this._init();
+      Quagga.start();
+      this._started = true;
+      this.started.next(true);
+    }
+  }
+
+  stop() {
+    if (this._started) {
+      Quagga.stop();
+      this._started = false;
+      this.started.next(false);
+    }
+  }
+
+  restart() {
+    if (this._started) {
+      this.stop();
+      this.start();
+    }
+  }
+
+  onProcessed(result: any): any {
+    const drawingCtx = Quagga.canvas.ctx.overlay,
+      drawingCanvas = Quagga.canvas.dom.overlay;
+
+    if (result) {
+      if (result.boxes) {
+        drawingCtx.clearRect(
+          0,
+          0,
+          parseInt(drawingCanvas.getAttribute('width'), 10),
+          parseInt(drawingCanvas.getAttribute('height'), 10)
+        );
+        result.boxes
+          .filter(function(box) {
+            return box !== result.box;
+          })
+          .forEach(function(box) {
+            Quagga.ImageDebug.drawPath(
+              box,
+              {
+                x: 0,
+                y: 1,
+              },
+              drawingCtx,
+              {
+                color: 'green',
+                lineWidth: 2,
+              }
+            );
+          });
+      }
+
+      if (result.box) {
+        Quagga.ImageDebug.drawPath(
+          result.box,
+          {
+            x: 0,
+            y: 1,
+          },
+          drawingCtx,
+          {
+            color: '#00F',
+            lineWidth: 2,
+          }
+        );
+      }
+
+      if (result.codeResult && result.codeResult.code) {
+        Quagga.ImageDebug.drawPath(
+          result.line,
+          {
+            x: 'x',
+            y: 'y',
+          },
+          drawingCtx,
+          {
+            color: 'red',
+            lineWidth: 3,
+          }
+        );
+      }
+    }
+  }
+
+  onDetected(result) {
+    this.valueChanges.next(result);
+  }
+}
---
+++ src/app/components/barcode-scanner/stream-config.ts
@@ -0,0 +1,48 @@
+export const DEFAULT_CONFIG = {
+  inputStream: {
+    name: 'Live',
+    type: 'LiveStream',
+    target: null,
+    constraints: {
+      width: { min: 640 },
+      height: { min: 480 },
+      aspectRatio: { min: 1, max: 100 },
+      facingMode: 'environment', // or user
+    },
+    singleChannel: false, // true: only the red color-channel is read
+  },
+  locator: {
+    patchSize: 'medium',
+    halfSample: true,
+  },
+  locate: true,
+  numOfWorkers: 4,
+  decoder: {
+    readers: ['code_128_reader'],
+  },
+} as QuaggaConfig;
+
+export interface QuaggaConfig {
+  inputStream: {
+    name: string;
+    type: string;
+    target: any;
+    constraints: {
+      width: { min: number };
+      height: { min: number };
+      aspectRatio: { min: number; max: number };
+      facingMode: string; // or user
+      deviceId: string;
+    };
+    singleChannel: boolean; // true: only the red color-channel is read
+  };
+  locator: {
+    patchSize: string;
+    halfSample: boolean;
+  };
+  locate: boolean;
+  numOfWorkers: number;
+  decoder: {
+    readers: string[];
+  };
+}
