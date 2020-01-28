371f466ebb4764d396e35f1e031e7413e35a569e
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-01-28 12:06:29 +0100

    feat: add created pages route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './pages/auth/auth.guard';
+import { UnAuthGuard } from './pages/auth/un-auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'home',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+  },
+  {
+    path: 'auth',
+    canActivate: [UnAuthGuard],
+    loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthPageModule),
+  },
+  {
+    path: 'user-list',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./pages/user-list/user-list.module').then(m => m.UserListPageModule),
+  },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
