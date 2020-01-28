11e4d05a5c6b6451136978019ae9afd4005799db
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-01-28 12:07:15 +0100

    feat: initialize authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  authUnsubscribe: firebase.Unsubscribe;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private authService: AuthService
   ) {
     this.initializeApp();
+    this.authService.initializeAuthUser();
   }
 
   initializeApp() {
@@ -23,5 +28,13 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  ngOnInit() {
+    this.authUnsubscribe = this.authService.subscribeAuthStatus();
+  }
+
+  ngOnDestroy() {
+    this.authUnsubscribe();
   }
 }
